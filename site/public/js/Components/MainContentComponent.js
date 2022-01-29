ebfbba4b1ccc841e33cb439f2dc41af129b59931
Pau Ibáñez <pauibanez2001@gmail.com>
2022-01-29 16:27:12 +0100

    Added stylings for pokemon card and 8 placeholder cards

--- site/public/js/Components/MainContentComponent.js
+++ site/public/js/Components/MainContentComponent.js
@@ -21,36 +21,277 @@ class MainContentComponent extends Component {
       <section class="main-content__content-list">
         <div class="main-content__list-container">
           <article class="pokemon-card">
-      <div class="pokemon-card__hp">
-        <span class="hp-number">160</span>
-        HP
-      </div>
-      <img src="img/snorlax.png" alt="" class="pokemon-card__img" />
-      <img
-        src="img/pokemonground.png"
-        alt=""
-        class="pokemon-card__img--ground"
-      />
-      <section class="pokemon-card__main-info">
-        <h2 class="pokemin-card__name">Snorlax</h2>
-        <ul class="pokemon-card__type-holder">
-          <li class="type">Normal</li>
-          <li class="type">Fire</li>
-        </ul>
-      </section>
-      <section class="pokemon-card__stats">
-        <ul>
-          <li class="pokemon-stats-item">
-            <div class="stat-name">ID</div>
-            <div class="stat-value">143</div>
-            <div class="stat-name">Abilities</div>
-            <div class="stat-value">Immunity / Thick-fat</div>
-            <div class="stat-name">Atack</div>
-            <div class="stat-value">110</div>
-          </li>
-        </ul>
-      </section>
-    </article>
+            <div class="pokemon-card__hp">
+              <span class="hp-number">160</span>
+              HP
+            </div>
+            <img
+              src="img/ground.png"
+              alt=""
+              class="pokemon-card__img--ground"
+            />
+            <img src="img/snorlax.png" alt="" class="pokemon-card__img" />
+            
+            <section class="pokemon-card__main-info">
+              <h2 class="pokemin-card__name">Snorlax</h2>
+              <ul class="pokemon-card__type-holder">
+                <li class="type">Normal</li>
+                <li class="type">Fire</li>
+              </ul>
+            </section>
+            <ul class="pokemon-card__stats">
+              <li class="pokemon-stats-item">
+                <div class="stat-name">ID</div>
+                <div class="stat-value">143</div>
+              </li>
+              <li class="pokemon-stats-item">
+                <div class="stat-name">Abilities</div>
+                <div class="stat-value">Immunity / Thick-fat</div>
+              </li>
+              <li class="pokemon-stats-item">
+                <div class="stat-name">Atack</div>
+                <div class="stat-value">110</div>
+              </li> 
+            </ul>
+          </article>
+          <article class="pokemon-card">
+            <div class="pokemon-card__hp">
+              <span class="hp-number">160</span>
+              HP
+            </div>
+            <img
+              src="img/ground.png"
+              alt=""
+              class="pokemon-card__img--ground"
+            />
+            <img src="img/snorlax.png" alt="" class="pokemon-card__img" />
+            
+            <section class="pokemon-card__main-info">
+              <h2 class="pokemin-card__name">Snorlax</h2>
+              <ul class="pokemon-card__type-holder">
+                <li class="type">Normal</li>
+                <li class="type">Fire</li>
+              </ul>
+            </section>
+            <ul class="pokemon-card__stats">
+              <li class="pokemon-stats-item">
+                <div class="stat-name">ID</div>
+                <div class="stat-value">143</div>
+              </li>
+              <li class="pokemon-stats-item">
+                <div class="stat-name">Abilities</div>
+                <div class="stat-value">Immunity / Thick-fat</div>
+              </li>
+              <li class="pokemon-stats-item">
+                <div class="stat-name">Atack</div>
+                <div class="stat-value">110</div>
+              </li> 
+            </ul>
+          </article>
+          <article class="pokemon-card">
+            <div class="pokemon-card__hp">
+              <span class="hp-number">160</span>
+              HP
+            </div>
+            <img
+              src="img/ground.png"
+              alt=""
+              class="pokemon-card__img--ground"
+            />
+            <img src="img/snorlax.png" alt="" class="pokemon-card__img" />
+            
+            <section class="pokemon-card__main-info">
+              <h2 class="pokemin-card__name">Snorlax</h2>
+              <ul class="pokemon-card__type-holder">
+                <li class="type">Normal</li>
+                <li class="type">Fire</li>
+              </ul>
+            </section>
+            <ul class="pokemon-card__stats">
+              <li class="pokemon-stats-item">
+                <div class="stat-name">ID</div>
+                <div class="stat-value">143</div>
+              </li>
+              <li class="pokemon-stats-item">
+                <div class="stat-name">Abilities</div>
+                <div class="stat-value">Immunity / Thick-fat</div>
+              </li>
+              <li class="pokemon-stats-item">
+                <div class="stat-name">Atack</div>
+                <div class="stat-value">110</div>
+              </li> 
+            </ul>
+          </article>
+          <article class="pokemon-card">
+            <div class="pokemon-card__hp">
+              <span class="hp-number">160</span>
+              HP
+            </div>
+            <img
+              src="img/ground.png"
+              alt=""
+              class="pokemon-card__img--ground"
+            />
+            <img src="img/snorlax.png" alt="" class="pokemon-card__img" />
+            
+            <section class="pokemon-card__main-info">
+              <h2 class="pokemin-card__name">Snorlax</h2>
+              <ul class="pokemon-card__type-holder">
+                <li class="type">Normal</li>
+                <li class="type">Fire</li>
+              </ul>
+            </section>
+            <ul class="pokemon-card__stats">
+              <li class="pokemon-stats-item">
+                <div class="stat-name">ID</div>
+                <div class="stat-value">143</div>
+              </li>
+              <li class="pokemon-stats-item">
+                <div class="stat-name">Abilities</div>
+                <div class="stat-value">Immunity / Thick-fat</div>
+              </li>
+              <li class="pokemon-stats-item">
+                <div class="stat-name">Atack</div>
+                <div class="stat-value">110</div>
+              </li> 
+            </ul>
+          </article>
+                    <article class="pokemon-card">
+            <div class="pokemon-card__hp">
+              <span class="hp-number">160</span>
+              HP
+            </div>
+            <img
+              src="img/ground.png"
+              alt=""
+              class="pokemon-card__img--ground"
+            />
+            <img src="img/snorlax.png" alt="" class="pokemon-card__img" />
+            
+            <section class="pokemon-card__main-info">
+              <h2 class="pokemin-card__name">Snorlax</h2>
+              <ul class="pokemon-card__type-holder">
+                <li class="type">Normal</li>
+                <li class="type">Fire</li>
+              </ul>
+            </section>
+            <ul class="pokemon-card__stats">
+              <li class="pokemon-stats-item">
+                <div class="stat-name">ID</div>
+                <div class="stat-value">143</div>
+              </li>
+              <li class="pokemon-stats-item">
+                <div class="stat-name">Abilities</div>
+                <div class="stat-value">Immunity / Thick-fat</div>
+              </li>
+              <li class="pokemon-stats-item">
+                <div class="stat-name">Atack</div>
+                <div class="stat-value">110</div>
+              </li> 
+            </ul>
+          </article>
+          <article class="pokemon-card">
+            <div class="pokemon-card__hp">
+              <span class="hp-number">160</span>
+              HP
+            </div>
+            <img
+              src="img/ground.png"
+              alt=""
+              class="pokemon-card__img--ground"
+            />
+            <img src="img/snorlax.png" alt="" class="pokemon-card__img" />
+            
+            <section class="pokemon-card__main-info">
+              <h2 class="pokemin-card__name">Snorlax</h2>
+              <ul class="pokemon-card__type-holder">
+                <li class="type">Normal</li>
+                <li class="type">Fire</li>
+              </ul>
+            </section>
+            <ul class="pokemon-card__stats">
+              <li class="pokemon-stats-item">
+                <div class="stat-name">ID</div>
+                <div class="stat-value">143</div>
+              </li>
+              <li class="pokemon-stats-item">
+                <div class="stat-name">Abilities</div>
+                <div class="stat-value">Immunity / Thick-fat</div>
+              </li>
+              <li class="pokemon-stats-item">
+                <div class="stat-name">Atack</div>
+                <div class="stat-value">110</div>
+              </li> 
+            </ul>
+          </article>
+          <article class="pokemon-card">
+            <div class="pokemon-card__hp">
+              <span class="hp-number">160</span>
+              HP
+            </div>
+            <img
+              src="img/ground.png"
+              alt=""
+              class="pokemon-card__img--ground"
+            />
+            <img src="img/snorlax.png" alt="" class="pokemon-card__img" />
+            
+            <section class="pokemon-card__main-info">
+              <h2 class="pokemin-card__name">Snorlax</h2>
+              <ul class="pokemon-card__type-holder">
+                <li class="type">Normal</li>
+                <li class="type">Fire</li>
+              </ul>
+            </section>
+            <ul class="pokemon-card__stats">
+              <li class="pokemon-stats-item">
+                <div class="stat-name">ID</div>
+                <div class="stat-value">143</div>
+              </li>
+              <li class="pokemon-stats-item">
+                <div class="stat-name">Abilities</div>
+                <div class="stat-value">Immunity / Thick-fat</div>
+              </li>
+              <li class="pokemon-stats-item">
+                <div class="stat-name">Atack</div>
+                <div class="stat-value">110</div>
+              </li> 
+            </ul>
+          </article>
+          <article class="pokemon-card">
+            <div class="pokemon-card__hp">
+              <span class="hp-number">160</span>
+              HP
+            </div>
+            <img
+              src="img/ground.png"
+              alt=""
+              class="pokemon-card__img--ground"
+            />
+            <img src="img/snorlax.png" alt="" class="pokemon-card__img" />
+            
+            <section class="pokemon-card__main-info">
+              <h2 class="pokemin-card__name">Snorlax</h2>
+              <ul class="pokemon-card__type-holder">
+                <li class="type">Normal</li>
+                <li class="type">Fire</li>
+              </ul>
+            </section>
+            <ul class="pokemon-card__stats">
+              <li class="pokemon-stats-item">
+                <div class="stat-name">ID</div>
+                <div class="stat-value">143</div>
+              </li>
+              <li class="pokemon-stats-item">
+                <div class="stat-name">Abilities</div>
+                <div class="stat-value">Immunity / Thick-fat</div>
+              </li>
+              <li class="pokemon-stats-item">
+                <div class="stat-name">Atack</div>
+                <div class="stat-value">110</div>
+              </li> 
+            </ul>
+          </article>
         </div>
         <div class="main-content__controls">
           <button class="button">&lt;&lt; Previous</button>
