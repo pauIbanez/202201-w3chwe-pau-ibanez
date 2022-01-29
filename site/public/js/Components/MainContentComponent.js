a935e0004ad138f7859828be4834d3d6da92a16e
Pau Ibáñez <pauibanez2001@gmail.com>
2022-01-29 14:52:37 +0100

    Added initial logic for MainContentComponent

---
+++ site/public/js/Components/MainContentComponent.js
@@ -0,0 +1,35 @@
+import Component from "./Component";
+
+class MainContentComponent extends Component {
+  data;
+
+  constructor(parentElement, className, htmlTag, data) {
+    super(parentElement, className, htmlTag);
+
+    this.data = data;
+    this.generateHTML();
+  }
+
+  generateHTML() {
+    this.element.innerHTML = `
+      <header class="main-content__content-header">
+        <h1 class="main-content__title">${this.data.description}</h1>
+        <p class="main-content__desc">
+          ${this.data.description}
+        </p>
+      </header>
+      <section class="main-content__content-list">
+        <div class="main-content__list-container">
+          <img src="" alt="" class="list_item" />
+        </div>
+        <div class="main-content__controls">
+          <button class="button">&lt;&lt; Previous</button>
+          <div class="position">80/10000</div>
+          <button class="button">Next >></button>
+        </div>
+      </section>
+    `;
+  }
+}
+
+export default MainContentComponent;
