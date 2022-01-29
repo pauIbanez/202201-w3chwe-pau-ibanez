ae86096a04a4457917e483fc85807afbf35bba3c
Pau Ibáñez <pauibanez2001@gmail.com>
2022-01-29 12:06:31 +0100

    Added test load without exiting index.html

--- site/public/js/index.js
+++ site/public/js/index.js
@@ -2,6 +2,16 @@
 
 import PokemonCardComponent from "./Components/PokemonCardComponent.js";
 
+const myPoke = document.querySelector(".main-nav__item:nth-child(2)");
+
+myPoke.addEventListener("click", (event) => {
+  event.preventDefault();
+  const xmlhttp = new XMLHttpRequest();
+  xmlhttp.open("GET", "html/mypokemon.html", false);
+  xmlhttp.send();
+  document.body.innerHTML = xmlhttp.responseText;
+});
+
 const container = document.querySelector(".main-content__list-container");
 
 const func = async () => {
