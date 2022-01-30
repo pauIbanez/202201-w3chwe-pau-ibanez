4079bab8740f9384b98ee28e93970d75b3925e9e
Pau Ibáñez <pauibanez2001@gmail.com>
2022-01-30 18:21:52 +0100

    Added basic details structure

--- site/public/js/Components/PokemonDetailsMainComponent.js
+++ site/public/js/Components/PokemonDetailsMainComponent.js
@@ -18,7 +18,24 @@ class PokemonDetailsMainComponent extends Component {
             <img class="details-holder__ground"src="../img/ground.png" />
             <img class="details-holder__img"src="${this.pokemonData.img}" />
           </div>
-          <div class="details-holder__info-holder"></div>
+          <div class="details-holder__info-holder">
+            <h2 class="details-holder__name"> ${
+              this.pokemonData.name[0].toUpperCase() +
+              this.pokemonData.name.substring(1)
+            }</h2>
+            <section class="details-holder__item details-holder__item--types">
+              <h3 class="title">Types</h3>
+              <ul class="list"></ul>
+            </section>
+            <section class="details-holder__item details-holder__item--stats">
+              <h3 class="title">Stats</h3>
+              <ul class="list"></ul>
+            </section>
+            <section class="details-holder__item details-holder__item--moves">
+              <h3 class="title">Moves</h3>
+              <ul class="list"></ul>
+            </section>
+          </div>
         </div>
         <div class="details-holder__controls"></div>
     `;
